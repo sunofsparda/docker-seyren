38faa58e10213ac01f8e80ed4f5ec9b6379a4114
Anton Cherlyonok <acherlyonok@gmail.com>
2017-11-17 18:33:04 +0300

    updated

--- run-seyren.sh
+++ run-seyren.sh
@@ -10,8 +10,6 @@ if [ -z ${GRAPHITE_URL} ]
        export GRAPHITE_URL=$1
 fi
 
-export MONGO_URL=mongodb://$MONGODB_PORT_27017_TCP_ADDR:$MONGODB_PORT_27017_TCP_PORT/seyren
-
 echo "Graphite URL $GRAPHITE_URL"
 echo "Mongo URL $MONGO_URL"
 
